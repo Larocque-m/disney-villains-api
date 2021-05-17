626b0c09ad2f18472486d70cc4ef50b258375c8e
Michael <mlarock711@comcast.net>
2021-05-16 22:35:04 -0400

    Beginning table in sql file. Need to set up connection.

--- Dianey-Villains Database.sql
+++ Dianey-Villains Database.sql
@@ -1,2 +1,12 @@
-create database Disney-Villains;
+create database DisneyVillains;
 
+use DisneyVillains; 
+create table wrongdoers (
+name varchar(255),
+movie varchar(255),
+slug varchar(255),
+createdAt datetime default current_timestamp, 
+updatedAt datetime default current_timestamp on update current_timestamp, 
+deletedAt datetime, 
+Primary Key(name)
+);
