74ce6a328224a07a8a48a35f6cf3195f5fe3fb97
Michael <mlarock711@comcast.net>
2021-05-25 01:57:19 -0400

    change to controllers. Add devs to villains test. Mocks for villains is complete.

---
+++ test/controller/villains.test.js
@@ -0,0 +1,7 @@
+const chai = require('chai')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+const { describe, it } = require('mocha')
+const { getAllVillains, getVillainsBySlug, saveNewVillain } = require('../../controllers/villains')
+const models = require('../../models')
+const { singleVillain, postedVillain, villainsList } = require('../mocks/villains')
---
+++ test/mocks/villains.js
@@ -0,0 +1,27 @@
+const singleVillain = {
+  name: 'Queen of Hearts',
+  movie: 'Alice in Wonderland',
+  slug: 'queen-of-hearts',
+}
+
+const villainsList = [{
+  name: 'Maleficent',
+  movie: 'Sleeping Beauty',
+  slug: 'maleficent',
+}, {
+  name: 'Scar',
+  movie: 'The Lion King',
+  slug: 'scar',
+}, {
+  name: 'Queen Grimhilde',
+  movie: 'Snow White and the Seven Dwarfs',
+  slug: 'queen-grimhilde',
+}]
+
+const postedVillain = {
+  name: 'Jafar',
+  movie: 'Aladdin',
+  slug: 'jafar',
+}
+
+module.exports = { singleVillain, villainsList, postedVillain }
